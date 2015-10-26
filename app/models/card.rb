1e1f0ba70a909e9dab754423918f7e1cab2b2ae8
Anthony+Chang <anthony.sh.chang@gmail.com>
2015-10-25 23:04:42 -0400

    Adds color and card_type to card serializer

--- app/models/card.rb
+++ app/models/card.rb
@@ -5,9 +5,13 @@ class Card < ActiveRecord::Base
 
 	belongs_to :deck, inverse_of: :cards
 	has_many :join_card_types, dependent: :destroy
+	has_many :card_types, through: :join_card_types
 	has_many :join_colors, dependent: :destroy
+	has_many :colors, through: :join_colors
 	has_many :join_sub_types, dependent: :destroy
+	has_many :sub_types, through: :join_sub_types
 	has_many :join_super_types, dependent: :destroy
+	has_many :super_types, through: :join_super_types
 
 	before_create :create_join_card_types
 
--- app/serializers/api/card_serializer.rb
+++ app/serializers/api/card_serializer.rb
@@ -1,5 +1,14 @@
 class Api::CardSerializer < ActiveModel::Serializer
 	self.root = false
 
-	attributes :id, :deck_id, :name, :cmc, :image_url, :toughness, :text, :mana_cost, :power, :quantity
+	attributes :id, :deck_id, :name, :cmc, :image_url, :toughness, :text, :mana_cost, :power, :quantity,
+		:colors, :card_types
+
+	def colors
+		object.colors.pluck(:name)
+	end
+
+	def card_types
+		object.card_types.pluck(:name)
+	end
 end
