d1aab30ce608164effc2a4d4527df70dc1a9230c
Anthony+Chang <anthony.sh.chang@gmail.com>
2015-10-26 17:14:37 -0400

    Adds comparator for card collection

--- app/assets/javascripts/collections/card.js
+++ app/assets/javascripts/collections/card.js
@@ -1,4 +1,11 @@
 Deckster.Collections.Card = Backbone.Collection.extend({
 	url: "api/cards",
 	
+	comparator: function(card) {
+		return card.get("cmc");
+
+		// TODO advanced comparator by
+		// 1) color order
+		// 2) color density
+	}
 });
