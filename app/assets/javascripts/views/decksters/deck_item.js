aee1b80863f4bbc85c77dde04f2fd88670c1f492
Anthony+Chang <anthony.sh.chang@gmail.com>
2015-10-27 20:24:40 -0400

    Adds color distribution chart

--- app/assets/javascripts/views/decksters/deck_item.js
+++ app/assets/javascripts/views/decksters/deck_item.js
@@ -10,14 +10,30 @@ Deckster.Views.deckItemView = Backbone.CompositeView.extend({
 
 	events: {
 		"mouseenter .name": "toggleImage",
-		"mouseleave .name": "toggleImage"
+		"mouseleave .name": "toggleImage",
+		"click .deck-menu li": "toggleDeckView"
 	},
 
 	initialize: function(opts) {
 		this.idx = opts.idx;
 		this.categories = ["creature", "non-creature", "land"];
 		this.counts = {};
-		this.model.cards().each(this.categorizeCard.bind(this));
+		this.model.cards().each(this._categorizeCard.bind(this));
+		this.colorDistrib = this.model.get("color_distribution");
+		this.colors = {
+			"White": "#FFFF66",
+			"Blue": "#0000FF",
+			"Black": "#000000",
+			"Red": "#FF0000",
+			"Green": "#33CC33"
+		};
+		this.highlightColors = {
+			"White": "#FFFF94",
+			"Blue": "#4D4DFF",
+			"Black": "#4D4D4D",
+			"Red": "#FF4D4D",
+			"Green": "#70DB70"
+		};
 
 		this.listenTo(this.collection, "add", this.addCard);
 	},
@@ -49,7 +65,7 @@ Deckster.Views.deckItemView = Backbone.CompositeView.extend({
 
 	toggleImage: function(e) {
 		var eventType = e.type,
-			card = $(e.currentTarget),
+			card = this.$(e.currentTarget),
 			imageUrl = card.siblings(".image").attr("src"),
 			imageBox = this.$(this.ui.imageBox);
 
@@ -60,7 +76,51 @@ Deckster.Views.deckItemView = Backbone.CompositeView.extend({
 		}
 	},
 
-	categorizeCard: function(card) {
+	toggleDeckView: function(e) {
+		var viewType = this.$(e.currentTarget).data("view");
+
+		switch(viewType) {
+			case "deck":
+				this.$(".content-container").removeClass("hidden");
+				this.$(".chart-container").addClass("hidden");
+				this.$(this.ui.imageBox).removeClass("hidden");
+				break;
+			case "charts":
+				this.$(".chart-container").removeClass("hidden");
+				this.$(".content-container").addClass("hidden");
+				this.$(this.ui.imageBox).addClass("hidden");
+				this._renderColorDistribChart();
+				break;
+			default:
+				break;
+		}
+	},
+
+	_renderColorDistribChart: function() {
+		var formattedData = [],
+			context = document.getElementById("color-distrib-" + this.model.id).getContext("2d"),
+			colorDistribChart,
+			key,
+			data,
+			options = {
+				responsive: true,
+				animationEasing: "easeInOutQuint"
+			};
+
+		for(key in this.colorDistrib) {
+			data = {
+				value: this.colorDistrib[key],
+				color: this.colors[key],
+				highlight: this.highlightColors[key],
+				label: key
+			};
+			formattedData.push(data);
+		}
+
+		colorDistribChart = new Chart(context).Pie(formattedData, options);
+	},
+
+	_categorizeCard: function(card) {
 		var types = card.get("card_types"),
 			quant = card.get("quantity");
 
