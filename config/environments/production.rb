fa186fda6190854feb8e2906228af1c9098c7b56
Anthony+Chang <anthony.sh.chang@gmail.com>
2015-11-09 18:58:26 -0500

    Adds region to paperclip prod defaults

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,7 @@ Rails.application.configure do
     :s3_protocol => 'http',
     :bucket => ENV['S3_BUCKET'],
     :endpoint => "decksterprod.s3.amazonaws.com",
+    :region => "Oregon",
     :s3_credentials => {
       :access_key_id => ENV['S3_ACCESS_KEY_ID'],
       :secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
