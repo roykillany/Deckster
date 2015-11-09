464c2d665ee1535e15d8bb2937f2857829744c0a
Anthony+Chang <anthony.sh.chang@gmail.com>
2015-11-09 10:39:47 -0500

    Removes card_type from card table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151108163232) do
+ActiveRecord::Schema.define(version: 20151109153757) do
 
   create_table "card_types", force: :cascade do |t|
     t.string   "name",       null: false
