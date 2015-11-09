e069540de6b8e56cbb72622b822db6b54132558b
Anthony+Chang <anthony.sh.chang@gmail.com>
2015-11-09 10:03:18 -0500

    Removes faulty migration

--- db/migrate/20151019225550_remove_card_columns.rb
+++
@@ -1,8 +0,0 @@
-class RemoveCardColumns < ActiveRecord::Migration
-  def change
-  	remove_column :cards, :super_type
-  	remove_column :cards, :card_type
-  	remove_column :cards, :sub_type
-  	remove_column :cards, :color
-  end
-end
