d7242ed0345758e7fedcbe05301d564ad75d517c
Anthony+Chang <anthony.sh.chang@gmail.com>
2015-11-09 18:54:50 -0500

    Specify s3 prod endpoint

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,7 @@ Rails.application.configure do
     :storage => :s3,
     :s3_protocol => 'http',
     :bucket => ENV['S3_BUCKET'],
+    :endpoint => "decksterprod.s3.amazonaws.com",
     :s3_credentials => {
       :access_key_id => ENV['S3_ACCESS_KEY_ID'],
       :secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
