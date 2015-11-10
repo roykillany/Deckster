92d8c32a55f823691ec38578899b6146b5d29318
Anthony+Chang <anthony.sh.chang@gmail.com>
2015-11-09 19:00:33 -0500

    Changes endpoint and region for paperclip prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,8 +81,8 @@ Rails.application.configure do
     :storage => :s3,
     :s3_protocol => 'http',
     :bucket => ENV['S3_BUCKET'],
-    :endpoint => "decksterprod.s3.amazonaws.com",
-    :region => "Oregon",
+    :endpoint => "s3-us-west-2.amazonaws.com",
+    :region => "us-west-2",
     :s3_credentials => {
       :access_key_id => ENV['S3_ACCESS_KEY_ID'],
       :secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
