bd0c40c827358849e654dcfbe8174ac745ec380b
Anthony+Chang <anthony.sh.chang@gmail.com>
2015-11-11 20:01:31 -0500

    Adds endpoint to application

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Deckster
 
     config.paperclip_defaults = {
     	storage: :s3,
+      endpoint: ENV['ENDPOINT'],
     	s3_credentials: {
     		bucket: ENV["S3_BUCKET"],
     		access_key_id: ENV["S3_ACCESS_KEY_ID"],
